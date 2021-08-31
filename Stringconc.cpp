15b84d79dc77562dc90f66c667e7ab0672823874
Pragyan Khati <83399260+rampdedlizer@users.noreply.github.com>
2021-08-31 20:36:38 +0545

    Update Stringconc.cpp

--- Stringconc.cpp
+++ Stringconc.cpp
@@ -41,3 +41,72 @@ int main()
 	display(s3);
 	display(s4);
 }
+
+//Multiple Inheritance
+//Hybrid Inheritance
+
+#include<iostream>
+
+class student
+{
+protected:
+	int r;
+public:
+	void getn(int a) { r = a; }
+	void putn()
+	{
+		std::cout << "Roll Number : " << r << std::endl;
+	}
+};
+
+class test :public student
+{
+protected:
+	int m1, m2;
+public:
+	void getm(int a, int b)
+	{
+		m1 = a; m2 = b;
+	}
+	void putm()
+	{
+		std::cout << "Marks 1 : " << m1 << std::endl;
+		std::cout << "Marks 2 : " << m2 << std::endl;
+	}
+};
+
+class sports
+{
+protected:
+	int s;
+public:
+	void gets(int z) { s = z; }
+	void puts() { std::cout << "Sports : " << s << std::endl; }
+};
+
+class result :public sports, public test
+{
+protected:
+	int total;
+public:
+	void display()
+	{
+		putn();
+		putm();
+		puts();
+		
+		total = s + m1 + m2;
+		std::cout << "Total Marks : " << total << std::endl;
+	}
+};
+
+
+int main()
+
+{
+	result student1;
+	student1.getn(26);
+	student1.getm(80, 85);
+	student1.gets(80);
+	student1.display();
+}
